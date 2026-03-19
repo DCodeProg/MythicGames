e0c91dc36db56b98edc2c984db093f8e0eda5860
DCodeProg <danael.legrand@outlook.fr>
2026-03-19 10:57:44 +0100

    feat: update SOAP client requests to improve product creation and retrieval logging

--- soap/client.js
+++ soap/client.js
@@ -6,34 +6,6 @@ soap.createClient("http://localhost:8000/products?wsdl", {}, function (err, clie
     return;
   }
 
-  client.GetProducts({}, function (err, result) {
-    if (err) {
-      console.error(
-        "Error making SOAP request:",
-        err.response.status,
-        err.response.statusText,
-        err.body
-      );
-      return;
-    }
-    console.log("Result:", result);
-  });
-
-
-  // Make a SOAP request with missing parameters
-  client.CreateProduct({ name: "My product" }, function (err, result) {
-    if (err) {
-      console.error(
-        "Error making SOAP request:",
-        err.response.status,
-        err.response.statusText,
-        err.body
-      );
-      return;
-    }
-    console.log("Result:", result);
-  });
-
   // Make a SOAP request with all parameters
   client.CreateProduct({ name: "My product", about: "This is a great product", price: 19.99 }, function (err, result) {
     if (err) {
@@ -45,7 +17,32 @@ soap.createClient("http://localhost:8000/products?wsdl", {}, function (err, clie
       );
       return;
     }
-    console.log("Result:", result);
+    console.log("Create Product result:", result);
   });
 
+  client.GetProducts({}, function (err, result) {
+    if (err) {
+      console.error(
+        "Error making SOAP request:",
+        err.response.status,
+        err.response.statusText,
+        err.body
+      );
+      return;
+    }
+    console.log("Get Products result:", result);
+  });
+
+  client.PatchProduct({ id: 3, name: "Updated product name", about: "Updated product description", price: 29.99 }, function (err, result) {
+    if (err) {
+      console.error(
+        "Error making SOAP request:",
+        err.response.status,
+        err.response.statusText,
+        err.body
+      );
+      return;
+    }
+    console.log("Patch Product result:", result);
+  });
 });
