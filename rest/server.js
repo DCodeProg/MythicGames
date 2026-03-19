f22d289dfe356c2249640828ba3bbf63e2f8d393
DCodeProg <danael.legrand@outlook.fr>
2026-03-19 11:49:08 +0100

    feat: update server configuration and implement product creation endpoint

--- rest/server.js
+++ rest/server.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const z = require("zod");
 const postgres = require("postgres");
 
-const sql = postgres({ db: "mydb", user: "user", password: "password" });
+const sql = postgres({ db: "mydb", user: "user", password: "password", port: 5433 });
+
+app.use(express.json());
 
 // Schemas
 const ProductSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  about: z.string(),
-  price: z.number().positive(),
+    id: z.string(),
+    name: z.string(),
+    about: z.string(),
+    price: z.number().positive(),
 });
 
-app.use(express.json());
+const CreateProductSchema = ProductSchema.omit({ id: true });
+
+app.post("/products", async (req, res) => {
+    const result = await CreateProductSchema.safeParse(req.body);
+
+    // If Zod parsed successfully the request body
+    if (result.success) {
+        const { name, about, price } = result.data;
+
+        const product = await sql`
+    INSERT INTO products (name, about, price)
+    VALUES (${name}, ${about}, ${price})
+    RETURNING *
+    `;
+
+        res.send(product[0]);
+    } else {
+        res.status(400).send(result);
+    }
+});
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -23,4 +45,3 @@ app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
 });
 
- 
