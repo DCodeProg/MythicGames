02e599c34883738df75b91393f1d2e56d617f2b0
DCodeProg <danael.legrand@outlook.fr>
2026-03-19 12:18:58 +0100

    feat: implement user creation endpoint with password encryption

--- rest/server.js
+++ rest/server.js
@@ -3,20 +3,30 @@ const app = express();
 const port = 8080;
 const z = require("zod");
 const postgres = require("postgres");
-
+const crypto = require("crypto");
 const sql = postgres({ db: "mydb", user: "user", password: "password", port: 5433 });
 
 app.use(express.json());
 
 // Schemas
 const ProductSchema = z.object({
-    id: z.string(),
+    id: z.int(),
     name: z.string(),
     about: z.string(),
     price: z.number().positive(),
 });
 
 const CreateProductSchema = ProductSchema.omit({ id: true });
+
+const UserSchema = z.object({
+    id: z.int(),
+    username: z.string(),
+    email: z.email(),
+    password: z.string(),
+});
+
+const CreateUserSchema = UserSchema.omit({ id: true });
+const UserResponseSchema = UserSchema.omit({ password: true });
 
 app.post("/products", async (req, res) => {
     const result = await CreateProductSchema.safeParse(req.body);
@@ -68,6 +78,25 @@ app.delete("/products/:id", async (req, res) => {
         res.send(product[0]);
     } else {
         res.status(404).send({ message: "Not found" });
+    }
+});
+
+app.post("/users", async (req, res) => {
+    const result = await CreateUserSchema.safeParse(req.body);
+
+    if (result.success) {
+        const { username, email, password } = result.data;
+        const encryptedPassword = crypto.createHash("sha512").update(password).digest("hex");
+
+        const user = await sql`
+    INSERT INTO users (username, email, password)
+    VALUES (${username}, ${email}, ${encryptedPassword})
+    RETURNING *
+    `;
+
+        res.send(UserResponseSchema.parse(user[0]));
+    } else {
+        res.status(400).send(result);
     }
 });
 
